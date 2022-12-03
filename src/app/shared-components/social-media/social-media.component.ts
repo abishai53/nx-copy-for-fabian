c8180bbb0fb372de0e30d38159f0c886444d1ce9
Ezra Orina <ezraorina53@gmail.com>
2022-12-03 03:45:48 +0000

    Migrate project from Angular CLI to nx

--- src/app/shared-components/social-media/social-media.component.ts
+++
@@ -1,8 +0,0 @@
-import {Component} from '@angular/core'
-
-@Component({
-    selector: 'ptf-social-media-icons',
-    templateUrl: 'social-media.component.html',
-    styleUrls: ['social-media.component.scss']
-})
-export class SocialMediaComponent {}
--- src/app/static-portfolio/navigable-page.ts
+++
@@ -1,5 +0,0 @@
-import {Navigation} from '../shared-components/utility-files/navigation'
-
-export interface NavigablePage {
-    pageName: Navigation
-}
