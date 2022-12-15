b08b662cc53daa8fffadda3b87209373d429cb4d
Ezra Orina <ezraorina53@gmail.com>
2022-12-15 17:28:14 -0600

    #10 linted files

--- apps/solution-portal-client/src/app/app.component.ts
+++ apps/solution-portal-client/src/app/app.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core'
 
 @Component({
-  selector: 'ezra-clients-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+    selector: 'slp-solution-portal-client-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'solution-portal-client';
+    title = 'solution-portal-client'
 }
--- apps/solution-portal-client/src/app/app.routes.ts
+++ apps/solution-portal-client/src/app/app.routes.ts
@@ -4,6 +4,6 @@ import {LandingPageComponent} from './landing-page/landing-page.component'
 
 export const appRoutes: Routes = [
     {path: '', redirectTo: SlpNavigation.LANDING_PAGE, pathMatch: 'full'},
-    {path: SlpNavigation.LANDING_PAGE, component: LandingPageComponent},
+    {path: SlpNavigation.LANDING_PAGE, component: LandingPageComponent}
 ]
 
