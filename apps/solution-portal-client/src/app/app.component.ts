3648335d58a872dcf27da925fc4cbd480cc58b96
Ezra Orina <ezraorina53@gmail.com>
2023-01-22 03:34:27 -0600

    made authentication service generic and shared

--- apps/solution-portal-client/src/app/app.component.ts
+++ apps/solution-portal-client/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import {Component, OnInit} from '@angular/core'
 import {SolutionsFacade} from './+state/solutions/solutions.facade'
-import {AuthService} from '@ezra-clients/common-ui'
+import {OktaService} from '@ezra-clients/common-ui'
 
 @Component({
     selector: 'slp-solution-portal-client-root',
@@ -9,9 +9,9 @@ import {AuthService} from '@ezra-clients/common-ui'
 })
 export class AppComponent implements OnInit {
     title = 'solution-portal-client'
-    name$ = this.authService.name$
+    name$ = this.oktaService.name$
 
-    constructor(private readonly solutionsFacade: SolutionsFacade, private readonly authService: AuthService) {}
+    constructor(private readonly solutionsFacade: SolutionsFacade, private readonly oktaService: OktaService) {}
 
     ngOnInit() {
         this.solutionsFacade.init()
