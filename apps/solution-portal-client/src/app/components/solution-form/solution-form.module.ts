1713e1b624de8738c0615358f2cc17b3cb51aba1
FabianGosebrink <fabian.gosebrink@outlook.com>
2023-01-29 15:46:29 +0100

    Reverting formatting in file changes 3

--- apps/solution-portal-client/src/app/components/solution-form/solution-form.module.ts
+++ apps/solution-portal-client/src/app/components/solution-form/solution-form.module.ts
@@ -1,33 +1,33 @@
-import { NgModule } from '@angular/core';
-import { SolutionFormComponent } from './solution-form.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { InputTextModule } from 'primeng/inputtext';
-import { InputTextareaModule } from 'primeng/inputtextarea';
-import { InputNumberModule } from 'primeng/inputnumber';
-import { DropdownModule } from 'primeng/dropdown';
-import { InputSwitchModule } from 'primeng/inputswitch';
-import { TooltipModule } from 'primeng/tooltip';
-import { NgClass, NgForOf, NgIf } from '@angular/common';
-import { MessageModule } from 'primeng/message';
-import { MessagesModule } from 'primeng/messages';
+import {NgModule} from '@angular/core'
+import {SolutionFormComponent} from './solution-form.component'
+import {FormsModule, ReactiveFormsModule} from '@angular/forms'
+import {InputTextModule} from 'primeng/inputtext'
+import {InputTextareaModule} from 'primeng/inputtextarea'
+import {InputNumberModule} from 'primeng/inputnumber'
+import {DropdownModule} from 'primeng/dropdown'
+import {InputSwitchModule} from 'primeng/inputswitch'
+import {TooltipModule} from 'primeng/tooltip'
+import {NgClass, NgForOf, NgIf} from '@angular/common'
+import {MessageModule} from 'primeng/message'
+import {MessagesModule} from 'primeng/messages'
 
 @NgModule({
-  declarations: [SolutionFormComponent],
-  exports: [SolutionFormComponent],
-  imports: [
-    ReactiveFormsModule,
-    FormsModule,
-    InputTextModule,
-    InputTextareaModule,
-    InputNumberModule,
-    DropdownModule,
-    InputSwitchModule,
-    TooltipModule,
-    NgIf,
-    NgForOf,
-    MessageModule,
-    NgClass,
-    MessagesModule,
-  ],
+    declarations: [SolutionFormComponent],
+    exports: [SolutionFormComponent],
+    imports: [
+        ReactiveFormsModule,
+        FormsModule,
+        InputTextModule,
+        InputTextareaModule,
+        InputNumberModule,
+        DropdownModule,
+        InputSwitchModule,
+        TooltipModule,
+        NgIf,
+        NgForOf,
+        MessageModule,
+        NgClass,
+        MessagesModule
+    ]
 })
 export class SolutionFormModule {}
