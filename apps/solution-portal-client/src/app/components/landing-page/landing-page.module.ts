42e329fe513a16c8b519a8391bb98644097987bf
FabianGosebrink <fabian.gosebrink@outlook.com>
2023-01-29 15:44:07 +0100

    reverted formatting changes 2

--- apps/solution-portal-client/src/app/components/landing-page/landing-page.module.ts
+++ apps/solution-portal-client/src/app/components/landing-page/landing-page.module.ts
@@ -1,27 +1,27 @@
-import { NgModule } from '@angular/core';
-import { LandingPageComponent } from './landing-page.component';
-import { InputTextModule } from 'primeng/inputtext';
-import { RouterLink } from '@angular/router';
-import { AsyncPipe, NgForOf, NgIf } from '@angular/common';
-import { SolutionWidgetModule } from '../solution-widget/solution-widget.module';
-import { MessageModule } from 'primeng/message';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { PipesModule } from '@ezra-clients/common-ui';
+import {NgModule} from '@angular/core'
+import {LandingPageComponent} from './landing-page.component'
+import {InputTextModule} from 'primeng/inputtext'
+import {RouterLink} from '@angular/router'
+import {AsyncPipe, NgForOf, NgIf} from '@angular/common'
+import {SolutionWidgetModule} from '../solution-widget/solution-widget.module'
+import {MessageModule} from 'primeng/message'
+import {FormsModule, ReactiveFormsModule} from '@angular/forms'
+import {PipesModule} from '@ezra-clients/common-ui'
 
 @NgModule({
-  declarations: [LandingPageComponent],
-  exports: [LandingPageComponent],
-  imports: [
-    InputTextModule,
-    RouterLink,
-    AsyncPipe,
-    NgForOf,
-    SolutionWidgetModule,
-    NgIf,
-    MessageModule,
-    FormsModule,
-    ReactiveFormsModule,
-    PipesModule,
-  ],
+    declarations: [LandingPageComponent],
+    exports: [LandingPageComponent],
+    imports: [
+        InputTextModule,
+        RouterLink,
+        AsyncPipe,
+        NgForOf,
+        SolutionWidgetModule,
+        NgIf,
+        MessageModule,
+        FormsModule,
+        ReactiveFormsModule,
+        PipesModule
+    ]
 })
 export class LandingPageModule {}
