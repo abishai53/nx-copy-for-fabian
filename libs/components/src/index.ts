e656dd69a341e0a5a6f1b740b54cbb5a77b4003f
FabianGosebrink <fabian.gosebrink@outlook.com>
2023-01-29 15:57:26 +0100

    reverting not needed changes

--- libs/components/src/index.ts
+++ libs/components/src/index.ts
@@ -1,5 +1,5 @@
-export * from './lib/button/button.component';
 export * from './lib/back-arrow/back-arrow.component';
+export * from './lib/button/button.component';
 export * from './lib/footer/footer.component';
 export * from './lib/form-buttons/form-buttons.component';
 export * from './lib/social-media/social-media.component';
